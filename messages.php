7290ea7a8fed4ad6744777f04f331030e4c59931
puuga <siwaoh@gmail.com>
2015-10-18 15:41:46 +0700

    update api
    can supply lat, lng

--- messages.php
+++ messages.php
@@ -3,27 +3,61 @@ header('Content-Type: application/json');
 ?>
 <?php include "include/db_connect_oo.php" ?>
 <?php
-  $sql = "SELECT m.id, m.message, m.lat, m.lng, m.created_at, u.name, u.facebook_id
-          FROM messages m INNER JOIN users u ON m.user_id = u.id
-          ORDER BY m.id DESC";
-  if ( isset($_GET['lat']) || isset($_GET['lng']) ) {
-    $sql .= "";
-  }
-  $result = $conn->query($sql);
-  if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-      $one = null;
-      $one['id'] = $row["id"];
-      $one['message'] = $row["message"];
-      $one['lat'] = $row["lat"];
-      $one['lng'] = $row["lng"];
-      $one['created_at'] = $row["created_at"];
-      $one['user']['name'] = $row["name"];
-      $one['user']['facebook_id'] = $row["facebook_id"];
 
-      $arr[] = $one;
+  function messages($conn) {
+    $sql = "SELECT m.id, m.message, m.lat, m.lng, m.created_at, u.name, u.facebook_id
+            FROM messages m INNER JOIN users u ON m.user_id = u.id
+            ORDER BY m.id DESC";
+    $result = $conn->query($sql);
+    $arr = array();
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $one = null;
+        $one['id'] = $row["id"];
+        $one['message'] = $row["message"];
+        $one['lat'] = $row["lat"];
+        $one['lng'] = $row["lng"];
+        $one['created_at'] = $row["created_at"];
+        $one['user']['name'] = $row["name"];
+        $one['user']['facebook_id'] = $row["facebook_id"];
+
+        $arr[] = $one;
+      }
+      // $json_result['messages'] = $arr;
     }
-    // $json_result['messages'] = $arr;
+
+    return $arr;
+  }
+
+  function messagesWithLatLng($conn, $lat, $lng) {
+    $sql = "CALL GetMessagesByMyLocation($lat,$lng)";
+    $result = $conn->query($sql);
+    $arr = array();
+    if ($result->num_rows > 0) {
+      while($row = $result->fetch_assoc()) {
+        $one = null;
+        $one['id'] = $row["id"];
+        $one['message'] = $row["message"];
+        $one['lat'] = $row["lat"];
+        $one['lng'] = $row["lng"];
+        $one['distance_from_my_location'] = $row["distance_from_my_location"];
+        $one['created_at'] = $row["created_at"];
+        $one['user']['name'] = $row["name"];
+        $one['user']['facebook_id'] = $row["facebook_id"];
+
+        $arr[] = $one;
+      }
+      // $json_result['messages'] = $arr;
+    }
+
+    return $arr;
+  }
+
+  // main
+  if ( isset($_GET['lat']) && isset($_GET['lng']) ) {
+    $arr = messagesWithLatLng($conn, $_GET['lat'], $_GET['lng']);
+  } else {
+    $arr = messages($conn);
   }
 
   $conn->close();
